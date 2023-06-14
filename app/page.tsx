bf5c718bee47fa77a93820a8ae80dfa2317f1746
Philipp Haack <PhilippHaackCode@gmail.com>
2023-06-14 10:00:31 +0200

    Added documentation refrences on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,40 @@
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <ThemeToggle />
+      <Button variant="outline">
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://nextjs.org/docs"
+        >
+          Nextjs Docs
+        </a>
+      </Button>
+      <Button variant="outline">
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://ui.shadcn.com"
+        >
+          Shadcn-ui Docs
+        </a>
+      </Button>
+      <Button variant="outline">
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://orm.drizzle.team/docs/quick-start"
+        >
+          Drizzle-ORM Docs
+        </a>
+      </Button>
     </section>
   )
 }
